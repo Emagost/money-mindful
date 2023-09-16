e1cb5bba9401c444b07155fcc0c3f5249d0b9074
Ema Gomez <emagozz@gmail.com>
2023-09-16 18:55:38 -0300

    Better security

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,17 +1,27 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
+let dbInstance: ReturnType<typeof getFirestore> | null = null;
+
 const firebaseConfig = {
-  apiKey: "AIzaSyBXpGxGixIpncAPIw40UCKgX9syLmzXKYI",
-  authDomain: "money-mindful-46a2e.firebaseapp.com",
-  projectId: "money-mindful-46a2e",
-  storageBucket: "money-mindful-46a2e.appspot.com",
-  messagingSenderId: "810584552781",
-  appId: "1:810584552781:web:391dc9a0ead7cc6265b304",
-  measurementId: "G-CQ5GZ8FGCM",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
+
+export const getDBInstance = () => {
+  if (!dbInstance) {
+    dbInstance = getFirestore(app);
+  }
+
+  return dbInstance;
+};
